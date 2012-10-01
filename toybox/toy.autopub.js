fb5e1f1d76500832e8df84f97ef48da1484a78f6
Leigh Garland <leigh+git@toychicken.co.uk>
2012-10-01 14:30:39 +0100

    Created a shortcut script for direct pub links

--- toybox/toy.autopub.js
+++ toybox/toy.autopub.js
@@ -7,19 +7,22 @@
 		myName = 'autopub', // this should always be included, you know, for completeness (and handy for passing into logs etc.)
 
 		 // private functions
-		doSomething,
+		doBindings,
+		pub,
 
 		// private detective
 		magnumPI;
 
 	// this function always destroys itself once it's initialised once
 	me.init = function () {
+		log('autopub init');
 		doBindings();
 		// this function always destroys itself once it's initialised once
-		me.init = function () {log('toy.' + myName + ' has already initialised')};
+		//me.init = function () {log('toy.' + myName + ' has already initialised')};
 	};
 
 	doBindings = function () {
+		log('autopub, doBindings');
 		$('body')
 			.on('click', '[data-toy-autopub]', pub)	
 			.on('touch', '[data-toy-autopub]', pub)	
@@ -32,6 +35,7 @@
 			topic = $el.data('toyAutopub'),
 			data = $el.data('toyAutomsg') || e;
 		_M.pub(topic, data);
+		log('pub', topic, data);
 	};
 
 	// ready to load motherflicker!
